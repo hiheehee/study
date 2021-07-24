71dbbbeae8764f94b4b24be703b31dfbe2689a59
hiheehee <gkek41@naver.com>
2021-07-24 18:41:40 +0900

    commit HTTP Method

---
+++ src/main/java/com/example/study/controller/GetController.java
@@ -0,0 +1,30 @@
+package com.example.study.controller;
+
+import com.example.study.model.SearchParam;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api")
+public class GetController {
+
+    @RequestMapping(method = RequestMethod.GET, path = "/getMethod") // localhost:8080/api/getMethod
+    public String getRequest(){
+        return "Hi getMethod";
+    }
+
+    @RequestMapping("/getParameter") // localhost:8080/api/getParameter?id=1234&password=abcd
+    public String getParameter(@RequestParam String id, @RequestParam(name = "password") String pwd){
+        System.out.println(id);
+        System.out.println(pwd);
+        return id+pwd;
+    }
+
+    @GetMapping("/getMultiParameter") // http://localhost:8080/api/getMultiParameter?account=asfd&email=sudy@gmail.com&page=10
+    public SearchParam getMultiParameter(SearchParam searchParam){
+        System.out.println(searchParam.getAccount());
+        System.out.println(searchParam.getEmail());
+        System.out.println(searchParam.getPage());
+
+        return searchParam;
+    }
+}
