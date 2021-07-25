cf605805bc6a496ecc2363cc5b83d0ac043516e0
hiheehee <gkek41@naver.com>
2021-07-25 15:04:01 +0900

    8-1-01. 사용자 API CREATE - 1

--- src/main/java/com/example/study/ifs/CrudInterface.java
+++ src/main/java/com/example/study/ifs/CrudInterface.java
@@ -2,12 +2,12 @@ package com.example.study.ifs;
 
 import com.example.study.model.network.Header;
 
-public interface CrudInterface {
-    Header create(); // todo request object 추가
+public interface CrudInterface<Req, Res> {
+    Header<Res> create(Req request); // todo request object 추가
 
-    Header read(Long id);
+    Header<Res> read(Long id);
 
-    Header update();
+    Header<Res> update(Req request);
 
     Header delete(Long id);
 }
---
+++ src/main/java/com/example/study/model/network/request/UserApiRequest.java
@@ -0,0 +1,34 @@
+package com.example.study.model.network.request;
+
+import com.example.study.model.entity.OrderGroup;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class UserApiRequest {
+
+    private Long id;
+
+    private String account;
+
+    private String password;
+
+    private String status;
+
+    private String email;
+
+    private String phoneNumber;
+}
